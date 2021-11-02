0082349d961fdcafd2a8770fa42846c9e36a3186
Saga Munkhbold <sagakhuraa@gmail.com>
2021-11-02 13:39:49 -0700

    added buttons and connected firebase

--- app/src/main/java/ca/bcit/assignment2/MainActivity.java
+++ app/src/main/java/ca/bcit/assignment2/MainActivity.java
@@ -3,6 +3,7 @@ package ca.bcit.assignment2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void monthClicked(View view) {
+
+    }
+
+    public void healthClicked(View view) {
+    }
+
+    public void genderClicked(View view) {
+    }
+
+    public void ageClicked(View view) {
     }
 }
